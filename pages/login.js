a157927d6b0cf09f95343e556ef91960993c6da2
Peter Eriksson <tuvoc1@hotmail.com>
2023-07-16 23:52:17 +0200

    Update login.js

--- pages/login.js
+++ pages/login.js
@@ -3,12 +3,12 @@ import { getProviders, signIn } from "next-auth/react";
 function Login({ providers }) {
   return (
     <div className="flex flex-col bg-black min-h-screen justify-center items-center">
-      <img
+      {/* <img
         className="w-52 mb-3"
         src="https://links.papareact.com/9xl"
         alt=""
         onClick={() => window.open("https://www.spotify.com")}
-      />
+      /> */}
       {Object.values(providers).map((provider, i) => (
         <div className="flex flex-col items-center" key={provider.name}>
           <h1 className="text-white text-2xl ">TrackTrends </h1>
