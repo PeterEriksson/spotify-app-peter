66cb2d05806171ed9ff89c6a6113ae0c7c15d842
Peter Eriksson <tuvoc1@hotmail.com>
2024-01-09 18:09:54 +0100

    Update SidebarOption.js

--- components/SidebarOption.js
+++ components/SidebarOption.js
@@ -6,11 +6,20 @@ import styles from "../styles/effects.module.css";
 function SidebarOption({ Icon, title, url, favoritedItems }) {
   const { asPath } = useRouter();
 
+  const [pressed, setPressed] = React.useState(false);
+
   return (
     <Link href={url}>
-      <div className={`  sidebarOption group  ${favoritedItems && "relative"}`}>
+      <div
+        onMouseDown={() => setPressed(true)}
+        onMouseUpCapture={() => setPressed(false)}
+        onMouseLeave={() => setPressed(false)}
+        className={`   sidebarOption group  ${favoritedItems && "relative"}`}
+      >
         <Icon
-          className={` ${styles.iconMobile}  sidebarIcon ${
+          className={`${
+            pressed && "xxs:scale-90 transform transition duration-100 ease-in"
+          } ${styles.iconMobile}  sidebarIcon ${
             asPath === url ? "text-spotifyGreen" : "text-gray-400"
           }  `}
         />
@@ -21,13 +30,16 @@ function SidebarOption({ Icon, title, url, favoritedItems }) {
         >
           {title}
         </p>
-        {favoritedItems && (
+
+        {favoritedItems?.length > 0 && (
           <div
             className={`${
               favoritedItems.length == 0 && "hidden"
-            } absolute top-0.5 left-2 xxs:left-10 rounded-full bg-red-500 h-[22px] w-[22px] flex items-center justify-center  `}
+            } pointer-events-none  absolute top-0.5 left-2 xxs:left-10 rounded-full bg-red-500 h-[22px] w-[22px] flex items-center justify-center  `}
           >
-            <p className={`text-sm text-white/75  `}>{favoritedItems.length}</p>
+            <p className={`text-sm text-white/75  pointer-events-none`}>
+              {favoritedItems.length}
+            </p>
           </div>
         )}
       </div>
