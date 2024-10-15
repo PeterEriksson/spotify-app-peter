c875faca2b6512cbc065d9640c6106b2ae64dcf2
Peter Eriksson <tuvoc1@hotmail.com>
2024-10-15 23:35:48 +0200

    Update Artist.js

--- components/Artist.js
+++ components/Artist.js
@@ -35,8 +35,8 @@ function Artist({ artist, discoverPage, artistsSelected, setArtistsSelected }) {
       //handleArtistClick fcn handles whether user is in discover or artists
       onClick={handleArtistClick}
       className={`card group cursor-pointer overflow-hidden text-white   ${
-        isArtistSelected() ? "border-spotifyGreen" : "border-black/70"
-      } ${discoverPage && "flex-shrink-0 pt-6 pb-2 px-2.5    "}  ${
+        isArtistSelected() ? "border-spotifyGreen" : "border-transparent"
+      } ${discoverPage && "flex-shrink-0 pt-6 pb-2 px-2.5 border   "}  ${
         discoverPage &&
         !isArtistSelected() &&
         artistsSelected.length < 3 &&
