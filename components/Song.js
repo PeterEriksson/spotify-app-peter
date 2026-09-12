8eed8cc0bdaa7401cb70b338603853952c611de3
Peter Eriksson <tuvoc1@hotmail.com>
2026-09-12 16:20:34 +0200

    Update Song.js

--- components/Song.js
+++ components/Song.js
@@ -278,10 +278,9 @@ function Song({ track, noPlay, wideDesign, nr }) {
         <div
           aria-label="ignore-pause"
           onClick={() => setShowAdditionalInfo((prev) => !prev)}
-          className={`absolute top-1.5 left-2.5 cursor-pointer text-gray-600 font-semibold text-xxs`}
+          className={`absolute top-1.5 left-2.5 cursor-pointer text-gray-400 font-semibold text-xxs`}
         >
-          show <br />
-          {showAdditionalInfo ? "less" : "more"}
+          ℹ
         </div>
 
         {/* HEART ICON */}
